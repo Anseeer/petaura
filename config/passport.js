ba992cb5c5bdd6ba3d9fbb4c75682c6adc245e6d
Anseeer <ahmedanseer@gmail.com>
2025-01-28 17:09:53 +0530

    updates

--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:"/auth/google/callback",
+      callbackURL:"https://petaura.site/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
